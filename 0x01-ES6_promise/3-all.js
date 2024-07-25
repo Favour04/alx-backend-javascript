fb61aee16b491618cc095c01768b88f6a8a327a4
Favour04 <idaeworfavour1@gmail.com>
2024-07-25 02:26:54 +0100

    First user created now

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
+export default async function handleProfileSignup() {
   try {
     const photo = uploadPhoto();
     const user = createUser();
-    Promise.all([photo, user])
+    await Promise.all([photo, user])
       .then((values) => {
         console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
       });
