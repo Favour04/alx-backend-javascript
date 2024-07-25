9106b5cdecdfed61373c9a112f4985cc029647ed
Favour04 <idaeworfavour1@gmail.com>
2024-07-25 01:11:00 +0100

    Using the then im promises wan whine me but i no panic

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error());
-    }
+  promise.then(() => {
+    console.log('Got a response from the API');
+    return { status: 200, body: 'success' };
   })
-  .then(() => console.log('Got a response from the API'));;
+  .catch(() => {
+    return new Error()
+  })
 }
