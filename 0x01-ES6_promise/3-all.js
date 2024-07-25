1c03a3d3de793be0fcfe459f0a75e190a071ea7a
Favour04 <idaeworfavour1@gmail.com>
2024-07-25 02:02:26 +0100

    First user created now

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,19 +1,19 @@
-import {uploadPhoto, createUser} from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-	const photo = uploadPhoto()
-	const user = createUser()
+  const photo = uploadPhoto();
+  const user = createUser();
 
-	user
-	.then((data) => {
-		const fname = data.firstName;
-		const lname = data.lastName;
+  user
+    .then((data) => {
+      const fname = data.firstName;
+      const lname = data.lastName;
 
-		photo
-		.then((data) => {
-			const photo = data.body;
-			console.log(`${photo} ${fname} ${lname}`)
-		})
-	})
-	.catch(() => console.log('Signup system offline'))
+      photo
+        .then((data) => {
+          const photo = data.body;
+          console.log(`${photo} ${fname} ${lname}`);
+        });
+    })
+    .catch(() => console.log('Signup system offline'));
 }
