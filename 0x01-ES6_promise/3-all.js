3394aabee23dae366004c02c33757613f6a69e84
Favour04 <idaeworfavour1@gmail.com>
2024-07-25 01:58:07 +0100

    First user created

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,18 @@
+import {uploadPhoto, createUser} from './utils';
+
+export default function handleProfileSignup() {
+	const photo = uploadPhoto()
+	const user = createUser()
+
+	user
+	.then((data) => {
+		const fname = data.firstName;
+		const lname = data.lastName;
+
+		photo
+		.then((data) => {
+			const photo = data.body;
+			console.log(`${photo} ${fname} ${lname}`)
+		})
+	})
+}
