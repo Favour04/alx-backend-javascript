5698643b9413cb741df426cce8b0edd81ce8b1c3
Favour04 <idaeworfavour1@gmail.com>
2024-07-27 03:42:18 +0100

    done

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,8 +1,8 @@
 export default function iterateThroughObject(reportWithIterator) {
-    let Str = '';
-    for (const rpx of reportWithIterator) {
-      Str += rpx;
-      if (rpx !== reportWithIterator[reportWithIterator.length - 1]) Str += ' | ';
-    }
-    return Str;
+  let Str = '';
+  for (const rpx of reportWithIterator) {
+    Str += rpx;
+    if (rpx !== reportWithIterator[reportWithIterator.length - 1]) Str += ' | ';
   }
+  return Str;
+}
